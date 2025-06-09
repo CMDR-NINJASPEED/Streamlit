a00508f18a828c47dd9a3ceb03cf349ce9d4f41a
CMDR-NINJASPEED <103883299+CMDR-NINJASPEED@users.noreply.github.com>
2025-06-09 09:18:40 +0000

    new file:   db/db_streamlit.py

---
+++ db/db_streamlit.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import sqlite3
+
+#Creamos el menú lateral
+st.sidebar.title("Menú lateral")
+opcion = st.sidebar.selectbox("Selecciona una opción", ["Ver usuarios:","Agregar","Actualizar","Eliminar"])
+# Creamos lista de usuario
+match opcion:
+    case "Ver usuarios":
+
+    case "Agregar"
+        st.text("Por favor, inserte los datos a crear")
+        st.text_input("Nombre:")
+        st.text_input("email")
+    st.input()
+    
+    case "Actualizar"
+
+    case "Eliminar"
