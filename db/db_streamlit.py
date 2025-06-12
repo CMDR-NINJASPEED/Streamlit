ed17a7a17b8a19e0507b285dc668268301d98661
CMDR-NINJASPEED <103883299+CMDR-NINJASPEED@users.noreply.github.com>
2025-06-12 09:43:12 +0000

    modified:   db/db_streamlit.py

--- db/db_streamlit.py
+++ db/db_streamlit.py
@@ -12,8 +12,8 @@ match opcion:
         st.text("Por favor, inserte los datos a crear")
         usuario=st.text_input("Nombre:")
         email=st.text_input("email")
-        st.button("Agregar")
-        if st.button("Agregar"):
+        prueba=st.button("Agregar")
+        if prueba:
             try:
                 # Conectar a la base de datos (crea el archivo si no existe)
                 conn = sqlite3.connect('usuarios.db')
